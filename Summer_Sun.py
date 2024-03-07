b86eac2f54e050a0c443b88f0c557d7ab3e861ec
woutbleyen <147030876+woutbleyen@users.noreply.github.com>
2024-03-07 10:02:39 +0100

    07/03/2024

--- Summer_Sun.py
+++ Summer_Sun.py
@@ -1,24 +1,3 @@
-# teken om ff te wachten = handig
-print("even wachten, we komen zo tot u")
-
-def calculate_score(avg_temp, snowfall):
-    score = 0
-    # hier kijk het programma naar de gemiddelde temp en gaat dan naarop kijken welke score hij moet geven
-    if avg_temp < 0:
-        score += 5
-    elif avg_temp >= 0 and avg_temp < 5:
-        score += 4
-    elif avg_temp >= 5 and avg_temp < 10:
-        score += 3
-    else:
-        score += 1
-
-    #
-    if snowfall > 5:
-        score = min(5, score + 1)
-
-    return score
-
 import os.path
 
 from google.auth.transport.requests import Request
@@ -27,9 +6,33 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 
 import base64
 from email.message import EmailMessage
-import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+# teken om ff te wachten = handig
+print("even wachten, we komen zo tot u")
+
+idealetemp = input("ideale tempratuur:")
+
+def ideale_temp(avg_temp):
+    if avg_temp < idealetemp:
+        break
+
+def calculate_score(avg_temp):
+    score = 0
+    # hier kijk het programma naar de gemiddelde temp en gaat dan naarop kijken welke score hij moet geven
+    if avg_temp < 0:
+        score += 5
+    elif 0 <= avg_temp < 5:
+        score += 4
+    elif 5 <= avg_temp < 10:
+        score += 3
+    else:
+        score += 1
+
+
+    return score
+
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
@@ -37,22 +40,22 @@ SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
 
 def main():
     coordinates = {
-        "Les Trois Vallées": {"lat": 45.3271, "lon": 6.5783},
-        "Sölden": {"lat": 46.9655, "lon": 11.0076},
-        "Chamonix-Mont Blanc": {"lat": 45.9237, "lon": 6.869},
-        "Val di Fassa": {"lat": 46.4424081, "lon": 11.6968477},
-        "Salzburger Sportwelt": {"lat": 46.8012, "lon": 9.2305},
-        "Alpenarena Flims-Laax-Falera": {"lat": 47.8095, "lon": 13.0550},
-        "Kitzsteinhorn Kaprun": {"lat": 47.1948, "lon": 10.1602},
-        "Ski Arlberg": {"lat": 45.4692, "lon": 6.9068},
-        "Espace Killy": {"lat": 50.7261, "lon": 15.6094}
+        "Ankara, Turkey": {"lat": 39.9334, "lon": 32.8597},
+        "Athens, Greece": {"lat": 37.9838, "lon": 23.7275},
+        "Valletta, Malta": {"lat": 35.8989, "lon": 14.5146},
+        "Sardinia, Italy": {"lat": 40.1209, "lon": 9.0129},
+        "Sicily, Italy": {"lat": 37.5990, "lon": 14.0154},
+        "Nicosia, Cyprus": {"lat": 35.1856, "lon": 33.3823},
+        "Mallorca, Spain": {"lat": 39.6953, "lon": 3.0176},
+        "Lagos, Portugal": {"lat": 37.1028, "lon": -8.6741},
+        "Mauritius": {"lat": -20.3484, "lon": 57.5522},
+        "Bucharest, Romania": {"lat": 44.4268, "lon": 26.1025}
 
     }
 
     import requests
 
     avg_temps = {}
-    snowfalls = {}
     scores = {}
     # voor ieder resort gaat het hier de coordinaten halen
     for resort, coord in coordinates.items():
@@ -60,29 +63,25 @@ def main():
         lon = coord["lon"]
         # dit is de api
         response = requests.get(
-            f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=50d520feae8bb9a6d968a0fa080baf1f&units=metric")
+            f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid"
+            f"=50d520feae8bb9a6d968a0fa080baf1f&units=metric")
         data = response.json()
 
         maxtemps = []
-        snowpdays = []
         # deze functie haal de info uit de maximum temperatuur en de sneewval uit de api json file
         for forecast in data['list']:
             if 'main' in forecast and 'temp_max' in forecast['main']:
                 maxtemps.append(forecast['main']['temp_max'])
-            if 'snow' in forecast:
-                if '3h' in forecast['snow']:
-                    snowpdays.append(forecast['snow']['3h'] / 10)
+
         # berekening van gemiddelde temp en sneeuwval
         avg_temp = sum(maxtemps) / len(maxtemps) if maxtemps else 0
-        tot_snow = sum(snowpdays)
+
         # dit steekt voor iedere resort de gem temp en de totale sneeuwval in dicionairies
         avg_temps[resort] = avg_temp
-        snowfalls[resort] = tot_snow
+
 
         # berekent voor ieder skigebied score op 5 en stopt het in een dict
-        scores[resort] = calculate_score(avg_temp, tot_snow)
-
-
+        scores[resort] = calculate_score(avg_temp)
 
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
@@ -122,14 +121,14 @@ def main():
         service = build("gmail", "v1", credentials=creds)
         result_data = ""
         for resort, avg_temp in avg_temps.items():
-            result_data += f"Skigebied: {resort}, Gemiddelde temp: {avg_temp}°C, Totale sneeuwval: {snowfalls[resort]}cm, Score: {scores[resort]}/5\n"
+            result_data += f"{resort}, Gemiddelde temp: {round(avg_temp)}°C, Score: {scores[resort]}/5\n\n"
 
         message = EmailMessage()
 
         message.set_content(result_data)
-# input mail
+        # input mail
         message["To"] = input("geef je email adres: ")
-        message["From"] = "mateodenys@gmail.com"
+        message["From"] = "woutbleyen@gmail.com"
         message["Subject"] = "SKi-aanbevelingen van Booking.com"
 
         # encoded message
@@ -151,6 +150,6 @@ def main():
         # TODO(developer) - Handle errors from gmail API.
         print(f"An error occurred: {error}")
 
+
 if __name__ == "__main__":
     main()
-
